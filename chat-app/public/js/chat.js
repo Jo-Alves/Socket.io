6b93da183e1b3ed1cdb8a9c61349c34d6a32e84d
jo-alves <joelmirogerio@gmail.com>
2022-09-25 10:11:57 -0300

    Socket.io Events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,15 @@
-io()
+const socket = io()
+
+socket.on("countUpdated", count => {
+    console.log("The count has been updated!", count)
+})
+
+document.getElementById("increment").addEventListener("click", () => {
+    console.log("clicked")
+    socket.emit("increment")
+})
+
+document.getElementById("decrement").addEventListener("click", () => {
+    console.log("clicked")
+    socket.emit("decrement")
+})
